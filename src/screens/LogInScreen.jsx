d9f7ebaf56fb66d5d29fba274c73e5ffc8b5a296
koji <admissions0718@gmail.com>
2021-06-08 01:14:13 +0900

    Implement signUp and LogIn

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import firebase from 'firebase';
 import Button from '../components/Button';
 
 export default function LogInScreen(props){
     const { navigation } = props;
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
+
+    function handlePress() {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredentaial) => {
+                const { user } = userCredentaial;
+                console.log(user.uid);
+                navigation.reset({
+                    index: 0,
+                    routes: [{ name: 'MemoList'}],
+            });
+            })
+            .catch(error => {
+                Alert.alert(error.code);
+            });
+
+    }
     return(
         <View style={styles.container}>
             <View style={styles.inner}>
@@ -32,12 +49,7 @@ export default function LogInScreen(props){
                 </View>
                 <Button
                     label="Submit"
-                    onPress={() => {
-                        navigation.reset({
-                            index: 0,
-                            routes: [{ name: 'MemoList'}],
-                    });
-                }}
+                    onPress={handlePress}
                 />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Not Resistered</Text>
--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+
+import firebase from 'firebase';
+
 import Button from '../components/Button';
 
 export default function SignUpScreen(props){
     const { navigation } = props;
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
+
+    function handlePress() {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            const { user } = userCredential;
+            console.log(user.uid);
+            navigation.reset({
+                index: 0,
+                routes: [{ name: 'MemoList'}],
+            });
+        })
+        .catch((error) => {
+            console.log(error.code, error.message);
+            Alert.alert(error.code);
+        });
+    }
     return(
         <View style={styles.container}>
             <View style={styles.inner}>
@@ -32,11 +51,7 @@ export default function SignUpScreen(props){
                 </View>
                 <Button
                     label="Submit"
-                        onPress={() => { navigation.reset({
-                            index: 0,
-                            routes: [{ name: 'MemoList'}],
-                        });
-                    }}
+                        onPress={handlePress}
                 />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Already Resistered</Text>
