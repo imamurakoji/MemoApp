0e31d3016fcc2149e10eb67b5cc6756230ffb488
koji <admissions0718@gmail.com>
2021-06-08 04:35:22 +0900

    Auto LogIn and LogOut Button

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -8,6 +8,18 @@ export default function LogInScreen(props){
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
 
+    useEffect(() => {
+        const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+            if(user){
+                navigation.reset({
+                    index: 0,
+                    routes: [{ name: 'MemoList'}],
+            });
+            }
+        });
+        return unsubscribe;
+    }, []);
+
     function handlePress() {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then((userCredentaial) => {
